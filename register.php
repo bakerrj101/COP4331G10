c834ba18312aa77cefceea9ec1198c6ca6d46c29
omarashry98 <81537403+omarashry98@users.noreply.github.com>
2022-09-01 19:35:51 -0400

    test file

---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+
+    if(isset($_POST['Register Now'])){
+        echo 'user submitted';
+        $Username = $_POST['input']
+        $Password = $_POST['createpass']
+        $RepeatPassword = $_POST['confirmpass']
+    }
+
+?>

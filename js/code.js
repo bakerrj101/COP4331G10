99c6759000aa0c064b387aa6aa6873a7359eb85a
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2022-09-09 13:24:58 -0400

    updated login/register and attached api

--- js/code.js
+++ js/code.js
@@ -1,15 +1,64 @@
+const urlBase = "http://g10contactmanager.live/LAMPAPI";
+const extension = "php";
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
 function toggle() {
   document.querySelector(".forms").classList.toggle("show-signup");
 }
 
 function doLogin() {
-  location.href = "main.html";
+  userId = 0;
+  firstName = "";
+  lastName = "";
+
+  let login = document.getElementById("loginName").value;
+  let password = document.getElementById("loginPassword").value;
+
+  document.getElementById("loginResult").innerHTML = "";
+
+  let tmp = { login: login, password: password };
+  //	var tmp = {login:login,password:hash};
+  let jsonPayload = JSON.stringify(tmp);
+
+  let url = urlBase + "/Login." + extension;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try {
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let jsonObject = JSON.parse(xhr.responseText);
+        userId = jsonObject.id;
+        console.log(userId);
+
+        if (userId < 1) {
+          document.getElementById("loginResult").innerHTML =
+            "User/Password combination incorrect";
+          return;
+        }
+
+        firstName = jsonObject.firstName;
+        lastName = jsonObject.lastName;
+
+        saveCookie();
+
+        location.href = "main.html";
+      }
+    };
+    xhr.send(jsonPayload);
+  } catch (err) {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
 }
 
 function doRegister() {
   let create = document.getElementById("createpass").value;
   let confirm = document.getElementById("confirmpass").value;
   if (create != confirm)
-    document.getElementById("loginResult").innerHTML =
+    document.getElementById("registerResult").innerHTML =
       "Passwords do not match!";
 }
